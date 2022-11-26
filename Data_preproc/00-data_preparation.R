9f214fc555543efc00b8c48ff302139cb9a35184
Vadim Maletskii <wiscain@yandex.ru>
2022-11-26 15:38:08 +0100

    Removing_sampling

--- Data_preproc/00-data_preparation.R
+++ Data_preproc/00-data_preparation.R
@@ -1,9 +1,8 @@
 library(tidyverse)
-set.seed(51)
-new <- ess %>% sample_n(., 1600)
 ess <- rio::import('Data_orig/ESS10.dta')
 new[new == '77'] <- NA
 new[new == '88'] <- NA
 new[new == '99'] <- NA
 pol_safe <- new %>% select(trstplc, aesfdrk)
 pol_safe <- na.omit(pol_safe)
+pol_safe
