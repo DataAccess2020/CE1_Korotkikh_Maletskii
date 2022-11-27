0c7abd8e864f059fe68117cec7b44718cf925647
skorotkikh <s.korotkikh21@gmail.com>
2022-11-27 10:53:01 +0100

    Creates the CSV file with the mean for each value of trstplc

--- Scripts/01-mean_of_sft.R
+++ Scripts/01-mean_of_sft.R
@@ -1,6 +1,13 @@
+# Importing the file with the data ----
+rio::import('Data_preproc/pol_safe.csv')
+
+# Counting the means for each value of trstplc ----
 mean_sft = pol_safe %>% 
   group_by(trstplc) %>%
   summarize(
     safty_mean = mean(aesfdrk, na.rm=T)
   )
 mean_sft
+
+# Creating the CSV file with the means ----
+rio::export(mean_sft, file = 'mean_sft', format = 'csv')
