67d35c9ee543f32da3963a2cc0a9aabe842838a4
skorotkikh <s.korotkikh21@gmail.com>
2022-11-27 10:42:28 +0100

    Moves the scripts to a different folder and creates the CSV file with the new data

--- Scripts/00-data_preparation.R
+++ Scripts/00-data_preparation.R
@@ -12,3 +12,6 @@ pol_safe <- ess %>% select(trstplc, aesfdrk)
 pol_safe <- na.omit(pol_safe)
 pol_safe <- pol_safe %>% mutate(aesfdrk = replace(aesfdrk, aesfdrk %in% c(7, 8, 9), NA))
 pol_safe <- na.omit(pol_safe)
+
+# Creating the CSV file with the new data ----
+rio::export(pol_safe, file = 'pol_safe', format = 'csv')
