896ca284adf263253e87bfaf723a915004cbdb07
Vadim Maletskii <wiscain@yandex.ru>
2022-11-27 13:35:57 +0100

    Technical stage

--- Data_preproc/00-data_preparation.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-ess <- rio::import('Data_orig/ESS10.dta')
-new[new == '77'] <- NA
-new[new == '88'] <- NA
-new[new == '99'] <- NA
-pol_safe <- new %>% select(trstplc, aesfdrk)
-pol_safe <- na.omit(pol_safe)
-pol_safe
