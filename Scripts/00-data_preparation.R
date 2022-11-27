1445382106cc2f3ff108601e708f55379797fd60
Vadim Maletskii <wiscain@yandex.ru>
2022-11-27 19:17:22 +0100

    Changing the numbers in a variable so that the higher the number the bigger feeling of safety

--- Scripts/00-data_preparation.R
+++ Scripts/00-data_preparation.R
@@ -13,5 +13,16 @@ pol_safe <- na.omit(pol_safe)
 pol_safe <- pol_safe %>% mutate(aesfdrk = replace(aesfdrk, aesfdrk %in% c(7, 8, 9), NA))
 pol_safe <- na.omit(pol_safe)
 
+# Changing the numbers in a variable so that the higher the number the bigger feeling of safety ----
+pol_safe <- pol_safe %>% mutate(aesfdrk = replace(aesfdrk, aesfdrk %in% c(1), 5))
+pol_safe <- pol_safe %>% mutate(aesfdrk = replace(aesfdrk, aesfdrk %in% c(2), 6))
+pol_safe <- pol_safe %>% mutate(aesfdrk = replace(aesfdrk, aesfdrk %in% c(3), 7))
+pol_safe <- pol_safe %>% mutate(aesfdrk = replace(aesfdrk, aesfdrk %in% c(4), 8))
+
+pol_safe <- pol_safe %>% mutate(aesfdrk = replace(aesfdrk, aesfdrk %in% c(5), 4))
+pol_safe <- pol_safe %>% mutate(aesfdrk = replace(aesfdrk, aesfdrk %in% c(6), 3))
+pol_safe <- pol_safe %>% mutate(aesfdrk = replace(aesfdrk, aesfdrk %in% c(7), 2))
+pol_safe <- pol_safe %>% mutate(aesfdrk = replace(aesfdrk, aesfdrk %in% c(8), 1))
+
 # Creating the CSV file with the new data ----
 rio::export(pol_safe, here::here("Data_preproc", 'pol_safe.csv'))
