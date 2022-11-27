9e0d3c8727ef2fef1a34c5e50dab976cb3543ccc
skorotkikh <s.korotkikh21@gmail.com>
2022-11-27 10:28:57 +0100

    Removes the NAs from the variable "aesfdrk"

--- Data_preproc/00-data_preparation.R
+++ Data_preproc/00-data_preparation.R
@@ -1,8 +1,14 @@
+# Loading packages ----
 library(tidyverse)
+
+# Importing the data and removing the NAs ----
 ess <- rio::import('Data_orig/ESS10.dta')
-new[new == '77'] <- NA
-new[new == '88'] <- NA
-new[new == '99'] <- NA
-pol_safe <- new %>% select(trstplc, aesfdrk)
+ess[ess == '77'] <- NA
+ess[ess == '88'] <- NA
+ess[ess == '99'] <- NA
+
+# Selecting the variables needed and removing the NAs ----
+pol_safe <- ess %>% select(trstplc, aesfdrk)
 pol_safe <- na.omit(pol_safe)
-pol_safe
+pol_safe <- pol_safe %>% mutate(aesfdrk = replace(aesfdrk, aesfdrk %in% c(7, 8, 9), NA))
+pol_safe <- na.omit(pol_safe)
